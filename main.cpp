938637d15fc848dfd02eac421028f59a672c7cd4
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-27 02:31:30 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{  
+
+int x = 2;
+int y = 4;
+
+if ((x > 1) && (y <= 4))
+  {cout << "True" << endl;}
+
+if ((y <= 4) && (x > 1))
+  {cout << "True";}
+
+return 0;
+}
